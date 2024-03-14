053c881898687ba81d77a6856679f5a1cace64c6
JadenLeeOC <19064@orewacollege.nz>
2024-03-15 11:06:46 +1300

    Update water_bottle_rocket.ino

--- water_bottle_rocket/water_bottle_rocket.ino
+++ water_bottle_rocket/water_bottle_rocket.ino
@@ -1,9 +1,40 @@
+#include "Arduino_SensorKit.h"
+#include <SparkFun_LPS25HB_Arduino_Library.h>
+LPS25HB pressureSensor;
+
 void setup() {
   // put your setup code here, to run once:
+ Serial.begin(9600);
+ Environment.begin();
+  Serial.println("LPS25HB Pressure Sensor Example 1 - Basic Readings");
+  Serial.println();
 
+  Wire.begin();
+  pressureSensor.begin(); // Begin links an I2C port and I2C address to the sensor, sets an I2C speed, begins I2C on the main board, and then sets default settings
+
+  if (pressureSensor.isConnected() == false) // The library supports some different error codes such as "DISCONNECTED"
+  {
+    Serial.println("LPS25HB disconnected. Reset the board to try again.");     // Alert the user that the device cannot be reached
+    Serial.println("Are you using the right Wire port and I2C address?");      // Suggest possible fixes
+    Serial.println("See Example2_I2C_Configuration for how to change these."); // Suggest possible fixes
+    Serial.println("");
+    while (1)
+      ;
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+Serial.print("Temperature = ");
+  Serial.print(Environment.readTemperature()); //print temperature
+  Serial.println(" C");
+  Serial.print("Humidity = ");
+  Serial.print(Environment.readHumidity()); //print humidity
+  Serial.println(" %");
+   Serial.print("Pressure in hPa: ");
+  Serial.print(pressureSensor.getPressure_hPa()); // Get the pressure reading in hPa
+  Serial.print(", Temperature (degC): ");
+  Serial.println(pressureSensor.getTemperature_degC()); 
+  delay(2000);
 
 }
