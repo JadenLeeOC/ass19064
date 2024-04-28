89ada3a208a1a1b4ee3bbe8d36ce1c5e3264b777
JadenLeeOC <19064@orewacollege.nz>
2024-04-29 10:13:51 +1200

    Update water_bottle_rocket.ino

--- water_bottle_rocket/water_bottle_rocket.ino
+++ water_bottle_rocket/water_bottle_rocket.ino
@@ -5,46 +5,47 @@
 #include <SPI.h>
 #include <SD.h>
 
-//TEACHER COMMENT what are these lines of code doing?
-//TEACHER COMMENT pressureSensor, and myLog are from the example code so need to be renamed to match our brief better
-LPS25HB pressureSensor;
-OpenLog myLog; //Create instance
+LPS25HB SensorPackage;  //Starts sensor
+OpenLog JadenCSV;       //Creates a new log
 
-//TEACHER COMMENT is this used anywhere? comment it out and see if any issues happen to check
-const byte OpenLogAddress = 42; //Default Qwiic OpenLog I2C address
+bool button = 4;
+int buttonstate = 0;
 
 void setup() {
+  buttonstate = digitalRead(4);
+  pinMode(4, INPUT);
 
   Wire.begin();
-  myLog.begin(); //Open connection to OpenLog (no pun intended)
+  JadenCSV.begin();  //Starts the connection to OpenLog
 
-  Serial.begin(9600); //9600bps is used for debug statements
-  //TEACHER COMMENT can you delete these next several lines of code? comment it out and see if any issues happen
-  Serial.println("Run OpenLog Append File Test");
-  myLog.println("Run OpenLog Append File Test");
-  Serial.begin(9600);
-  Serial.println("LPS25HB Pressure Sensor Example 1 - Basic Readings");
-  Serial.println();
-  myLog.println("This is recorded to the default log file");
-  myLog.println("This is recorded to JadenData.txt");
-  myLog.println("If this file doesn't exist it is created and");
-  myLog.println("anything sent to OpenLog will be recorded to this file");
+  Serial.begin(9600);  //9600bps is used for debug statements
 
- //TEACHER COMMENT add a comment to explain what are these line of code doing
-  myLog.append("JadenData.txt");
-  myLog.syncFile();
+  //TEACHER COMMENT add a comment to explain what are these line of code doing
+  JadenCSV.append("JadenData.txt");  // adds the log to the JadenData.txt file
+  JadenCSV.syncFile();
 
-//TEACHER COMMENT and what this one is doing - any checks you would do on mylog and pressuresensor to check they are working well?
-  pressureSensor.begin();  // Begin links an I2C port and I2C address to the sensor, sets an I2C speed, begins I2C on the main board, and then sets default settings
-  Serial.println("Done!");
+  //TEACHER COMMENT and what this one is doing - any checks you would do on Jadenlog and pressuresensor to check they are working well?
+
+  SensorPackage.begin();    // Begin links an I2C port and I2C address to the sensor, sets an I2C speed, begins I2C on the main board, and then sets default settings
+  Serial.println("Done!");  // prints a message to the serial monitor to show the user that the Pressure sensor is working properly and is connected to the I2C port
 }
 
 void loop() {
-//TEACHER COMMENT add an explaination of what this is doing - I would suggest looking at how the example csv file on classroom looks to check yours looks correct
-  myLog.print("Pressure in hPa: ");
-  myLog.print(pressureSensor.getPressure_hPa());  // Get the pressure reading in hPa
-  myLog.print(", Temperature (degC): ");
-  myLog.println(pressureSensor.getTemperature_degC());
-  delay(500);
+  //TEACHER COMMENT add an explaination of what this is doing - I would suggest looking at how the example csv file on classroom looks to check yours looks correct
+  if (digitalRead(button) == HIGH) {
 
+    JadenCSV.print("Pressure in hPa: ");                    // Adds text to show readings in a proper sentence
+    JadenCSV.print(SensorPackage.getPressure_hPa());        // Gets the pressure reading in hPa
+    JadenCSV.print(", Temperature (degC): ");               // Adds text to show readings in a proper sentence
+    JadenCSV.println(SensorPackage.getTemperature_degC());  // Gets the temperature reading in degrees celsius
+    Serial.print("Pressure in hPa: ");                      // Adds text to show readings in a proper sentence
+    Serial.print(SensorPackage.getPressure_hPa());          // Gets the pressure reading in hPa
+    Serial.print(", Temperature (degC): ");                 // Adds text to show readings in a proper sentence
+    Serial.println(SensorPackage.getTemperature_degC());    // Gets the temperature reading in degrees celsius
+    delay(500);                                             // adds a 5 second delay
+  } else if (digitalRead(button) == LOW) {
+
+    Serial.println("Sensor Package is Turned off, Turn Sensor Package On to Recieve Sensor Readings");
+    JadenCSV.println("Sensor Package is Turned off, Turn Sensor Package On to Recieve Sensor Readings");
+  }
 }
